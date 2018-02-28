acfbb0eb6480a9dd84bf14a1fa64c72d45b161df
Bryan Kai <brykai123@gmail.com>
2018-02-28 09:05:09 -0800

    Fixed side bar

---
+++ js/bryan.js
@@ -0,0 +1,13 @@
+
+  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'right', // Choose the horizontal origin
+      draggable: true, // Choose whether you can drag to open on touch screens,
+      }
+  );
+
+// Side Nav Bar
+ // Show sideNav
+ $('.button-collapse').sideNav('show');
+ // Hide sideNav
+ // $('.button-collapse').sideNav('hide');
